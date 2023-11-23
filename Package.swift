2cbf82a2297d82856bebe2ade11a65d874c9a9b8
Lennart Buchmann <43990440+lennbuch@users.noreply.github.com>
2023-11-23 13:51:04 +0100

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -14,6 +14,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+// swift-tools-version:5.9
 import PackageDescription
 
 let package = Package(
